e618b900d0be67bf30eb074913c81bdf9a464497
Yonexdu0ra <phamngocquy.7303@gmail.com>
2025-09-22 16:11:21 +0700

    cập nhật thêm phân loại quyền

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,33 +1,45 @@
 
 
 
+const ADMIN_ROLE = "Admin";
+const LIBRARIAN_ROLE = "Librarian";
+const READER_ROLE = "Reader";
+
 
 const requiredRoleLibraries = (req, res, next) => {
-    if (req.user.role !== "Librarian") {
+    if (req.user.role !== LIBRARIAN_ROLE) {
         return res.status(403).json({ error: "Forbidden" });
     }
     next();
 };
 
 const requiredRoleAdmin = (req, res, next) => {
-    if (req.user.role !== "Admin") {
+    if (req.user.role !== ADMIN_ROLE) {
         return res.status(403).json({ error: "Forbidden" });
     }
     next();
 };
 
 
-const requiredRoleReaderAndLibrarian = (req, res, next) => {
-    if (req.user.role !== "Reader" && req.user.role !== "Librarian") {
+const requiredRoleReaderOrLibrarian = (req, res, next) => {
+    if (req.user.role !== READER_ROLE && req.user.role !== LIBRARIAN_ROLE) {
         return res.status(403).json({ error: "Forbidden" });
     }
     next();
 };
 
+
+const requireRoleLibrarianOrAdmin = (req, res, next) => {
+    if (req.user.role !== LIBRARIAN_ROLE && req.user.role !== ADMIN_ROLE) {
+        return res.status(403).json({ error: "Forbidden" });
+    }
+    next();
+}
 
 
 module.exports = {
     requiredRoleLibraries,
     requiredRoleAdmin,
-    requiredRoleReaderAndLibrarian
+    requiredRoleReaderOrLibrarian,
+    requireRoleLibrarianOrAdmin
 };
