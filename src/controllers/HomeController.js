392d958b0776a7c0c3d52df1f46c6609e73d257b
Yonexdu0ra <phamngocquy.7393@gmail.com>
2025-10-02 15:15:24 +0000

    fix lỗi giao diện ở detail category và author và cập nhật giao diện phân trang

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,7 +2,7 @@ const BookService = require("../services/BookService");
 const CategoryService = require("../services/CategoryService");
 class HomeController {
     static async index(req, res) {
-        const { count: totalBooks, rows: books } = await BookService.getAllBooks({ page: 1, limit: 9 });
+        const { count: totalBooks, rows: books } = await BookService.getAllBooks({ page: 1, limit: 6 });
         const { count: totalCategories, rows: categories } = await CategoryService.getAllNoPagination();
         return res.render("index", { title: "Trang chủ", books, categories });
     }
